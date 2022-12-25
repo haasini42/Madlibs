f4d941b89e19803064da20a38903ea5733f63228
Haasini T <119388290+haasini42@users.noreply.github.com>
2022-12-25 13:04:59 +0530

    Add files via upload

---
+++ playmadlibs.py
@@ -0,0 +1,38 @@
+#1st paragraph 
+name_stadium =input("stadium name: ")
+team_name =input("team name: ")
+team_name2 =input("team name: ")
+num =input("number:")
+adj =input("Adjective: ")
+adj2 =input("Adjective: ")
+verb =input("Verb: ")
+verb2 =input("Verb: ")
+noun =input("Noun: ")
+
+#2nd paragraph
+player_name =input("player name: ")
+player_name2 =input("player name: ")
+player_name3 =input("player name: ")
+place_name =input("place name: ")
+place_name2 =input("place name: ")
+adj3 =input("Adjective: ")
+adj4 =input("Adjective: ")
+adj5 =input("Adjective: ")
+noun2 =input("Noun: ")
+noun3 =input("Noun: ")
+adv =input("Adverb: ")
+adv2 =input("Adverb: ")
+num2 =input("number: ")
+
+
+
+
+#1st paragraph
+madlibs1 = f"Last night at {name_stadium} Stadium, {team_name} played against {team_name2}. It was the {num} gmae of the Euro League chamionship.All the players were {adj}. The {adj2} coach screamed at the players that it was do or {verb}. If they {verb2} the game, then their {noun} would not be in the finals."
+
+
+#2nd paragraph
+madlibs = f"Our best player {player_name} is sick with swine flu and has to stay at the {place_name} to get {adj3}. So the coach chose {player_name2} to be the {noun2} of the team. He plays {adv} and usually misses all his {num2} point shots. The rest of the team is very {adj5}.They said that the team needed {player_name3} as he runs {adv2} dowm the {place_name2} and never misses {noun3}"
+
+print(madlibs1)
+print(madlibs)
